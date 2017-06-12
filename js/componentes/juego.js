99526a88171f04c097bc57e4167e614cf926bc99
Erika Vidal <diann852@gmail.com>
2017-06-12 11:31:46 -0500

    puntaje

--- js/componentes/juego.js
+++ js/componentes/juego.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-const empiezaJuego =(coder,update)=>{
+const empiezaJuego =(coder,mensaje,update)=>{
     const container     = $('<div class="container"></div>');
     const rowSelect     = $('<div class="row"></div>');
     const divSelect     = $('<div class="col-xs-12 seleccionar"></div>');
@@ -19,12 +19,13 @@ const empiezaJuego =(coder,update)=>{
     const row           = $('<div class="row hide"></div>');
     const divImg        = $('<div class="col-xs-6"></div>');
     const img           = $('<img class="img-responsive">');
-    const rowForm       = $('<div class="col-xs-6"></div>');
+    const rowForm       = $('<div class="col-xs-6 formulario"></div>');
     const form          = $('<form></form>');
     const div           = $('<div class="form-group"></div>');
     const label         = $('<label>Ingresa su nombre:</label>');
     const input         = $('<input type="text" class="form-control" id="nombre-coder" placeholder="Nombre">');
     const button        = $('<button type="submit" class="btn btn-primary">Comprobar</button>');
+    const mensajeResult = $('<p></p>');
 
     divSelect.append(sede);
     select.append(eligeOption);
@@ -38,6 +39,7 @@ const empiezaJuego =(coder,update)=>{
     div.append(label);
     div.append(input);
     div.append(button);
+    div.append(mensajeResult);
     form.append(div);
     rowForm.append(form);
     divImg.append(img);
@@ -46,40 +48,73 @@ const empiezaJuego =(coder,update)=>{
     container.append(rowSelect);
     container.append(row);
 
+    let selector;
     select.on("change",_=>{
+
+        selector=$("select option:selected").text();
         state.selectedCoder=coder;
+
         $(".hide").removeClass("hide");
-        mostrarImagen(select.val(),input.val(),state.coders,img,update);
-        //filtrado(select.val(),state.coders,img,update);
-        //console.log(select.val());
+
+        mostrarImagen(selector,input.val(),state.coders,img,update);
     })
 
     button.on("click",(e)=>{
+
         e.preventDefault();
-        mostrarImagen(select.val(),input.val(),state.coders,img,update);
+
+        const mensaje=muestraPuntaje(input.val(),state.coders,selector,img,update);
+
+        mensajeResult.append(mensaje);
+
     })
+
     return container;
 }
 
-const mostrarImagen=(valorSelect,inputVal,coder,img,update)=>{
-    let contador=0;
+const mostrarImagen=(valorSelect,inputVal,coder,img,update)=> {
 
-     coder.forEach((e)=> {
-        if (valorSelect === e.sede) {
-            img.attr("src",e.image);
+    const array = coder[Math.floor(Math.random() * coder.length)];
+    console.log(array);
 
-            return false;
-        }
-        if(inputVal.toLowerCase()===e.name.toLowerCase()){
+    if (valorSelect === array.sede) {
+
+        img.attr("src", array.image);
+
+    }
+    state.coders=array;
+
+}
+
+const muestraPuntaje = (inputVal,coder,selector,img,update)=>{
+    let puntaje = 0;
+    let contador = 0;
+    let mensaje = "";
+console.log(coder);
+    if(inputVal.toLowerCase()===coder.name.toLowerCase()){
+
+          puntaje++;
+
+          $(".puntaje").empty().append(puntaje);
+           mensaje= "Excelente";
+
+        mostrarImagen(selector,inputVal,state.coders,img,update);
+           return mensaje;
+
+        }else {
+
             contador++;
-            $(".puntaje").empty().append(contador);
-            return false;
-        }else{
-            contador--;
-            $(".puntaje").empty().append(contador);
-            return false;
+            mensaje= "Sigue intentando";
+
+            if (contador == 5) {
+
+                puntaje--;
+
+                $(".puntaje").empty().append(puntaje);
+
+                return mensaje;
+            }
         }
-    })
       // coder.forEach((element,i)=>{
            //const i = Math.floor(Math.random()*40);
 
